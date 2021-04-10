28c1a67bfc53f587ba8b1216e3ea23dbd98e18d2
SergeyBiryukov <twc2012@yandex.ru>
2021-04-10 18:19:46 +0300

    wave

---
+++ Zombie Atack/Assets/ZombieControlllwe.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
+
+public class ZombieControlllwe : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+    private void Death()
+    {
+        Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Bullet"))
+        {
+            Death();
+        }
+    }
+
+    private void Update()
+    {
+        var player = FindObjectOfType<Player>();
+        _agent.SetDestination(player.transform.position);
+    }
+}
