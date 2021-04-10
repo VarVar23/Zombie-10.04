fc782242c035e590b6104182ebbdf5ff16b32a69
NooB <oleg.nakhaevgame@mail.ru>
2021-04-10 18:52:35 +0300

    ADD SHIT

---
+++ Zombie Atack/Assets/Scripts/Player/Manager/AxisManager.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public static class AxisManager
+{
+    public readonly static string HORIZONTAL = "Horizontal";
+    public readonly static string VERTICAL = "Vertical";
+    public readonly static string JUMP = "Jump";
+}
+
---
+++ Zombie Atack/Assets/Scripts/Player/Move/MoveController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+public class MoveController : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _force;
+
+    private Rigidbody _rb;
+    private Vector3 _move;
+    private bool _ground;
+    private float _mouseX;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        Jump();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        RotationPlayer();
+    }
+
+    private void RotationPlayer()
+    {
+
+    }
+    private void Move()
+    {
+        float horizontal = Input.GetAxis(AxisManager.HORIZONTAL) * _speed;
+        float vertical = Input.GetAxis(AxisManager.VERTICAL) * _speed;
+
+        _move = new Vector3(horizontal, 0, vertical);
+        transform.Translate(_move * 0.01f);
+    }
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _ground)
+        {
+            _rb.AddForce(Vector3.up * _force);
+        }
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            _ground = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            _ground = false;
+        }
+    }
+}
